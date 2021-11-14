c2fad4d8105cc7b1e9d3ddd6b2893e04ba9fddad
Mateusz Choma <kontakt@coderoad.pl>
2021-11-14 17:30:06 +0100

    `Button` variants

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
 
+import { Button } from '@mui/material'
+
 export const App = () => {
   return (
     <div>
-      CodeRoad APP
+      <Button>
+        DEFAULT
+      </Button>
+      <br />
+      <Button
+        variant={'text'}
+      >
+        TEXT
+      </Button>
+      <br />
+      <Button
+        variant={'contained'}
+        color={'primary'}
+      >
+        CONTAINED PRIMARY
+      </Button>
+      <br />
+      <Button
+        variant={'contained'}
+        color={'secondary'}
+      >
+        CONTAINED SECONDARY
+      </Button>
+      <br />
+      <Button
+        variant={'outlined'}
+      >
+        OUTLINED
+      </Button>
     </div>
   )
 }
