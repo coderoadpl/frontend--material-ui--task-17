7d09dd4735a32fc59743bec573cb43f36971868d
Mateusz Choma <kontakt@coderoad.pl>
2021-11-16 11:53:49 +0100

    Using `styled` finction to style `BlackBox` component

--- src/components/BlackSquare/BlackSquare.js
+++ src/components/BlackSquare/BlackSquare.js
@@ -1,38 +1,15 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
 import { Box } from '@mui/material'
+import { styled } from '@mui/system'
 
-export const BlackSquare = (props) => {
-  const {
-    sx,
-    children,
-    ...otherProps
-  } = props
-
-  return (
-    <Box
-      sx={{
-        width: 100,
-        height: 100,
-        m: 2,
-        bgcolor: 'common.black',
-        color: 'common.white',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        ...sx
-      }}
-      {...otherProps}
-    >
-      {children}
-    </Box>
-  )
-}
-
-BlackSquare.propTypes = {
-  sx: PropTypes.object,
-  children: PropTypes.node
-}
+export const BlackSquare = styled(Box)(({ theme }) => ({
+  width: 100,
+  height: 100,
+  margin: theme.spacing(2),
+  backgroundColor: theme.palette.common.black,
+  color: theme.palette.common.white,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}))
 
 export default BlackSquare
